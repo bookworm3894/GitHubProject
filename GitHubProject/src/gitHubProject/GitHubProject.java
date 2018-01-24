16e56cb50fb4b778e3862c570b84e38c77516994
littertm <littertm@IL-PC-140.uccc.uc.edu>
2018-01-23 19:09:49 -0500

    Another Commit

---
+++ GitHubProject/src/gitHubProject/GitHubProject.java
@@ -0,0 +1,11 @@
+package gitHubProject;
+
+public class GitHubProject {
+
+	public static void main(String[] args) {
+		System.out.println(" This is the java project I created to practice uploading to GitHub repository. ");
+		
+
+	}
+
+}
